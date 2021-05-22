224fd29725f1eabc8fd4689a06ee4196e83992c5
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-05-22 20:51:04 -0300

    Parameters and prepared statement

--- pdo/create.php
+++ pdo/create.php
@@ -2,18 +2,32 @@
 
 include_once 'Database.php';
 
-$insertQuery = "INSERT INTO books(name, description, created_at)
-                VALUES('Introduction to Java', 'Learn all about Java', now())";
+// $insertQuery = "INSERT INTO books(name, description, created_at)
+//                 VALUES('Introduction to Java', 'Learn all about Java', now())";
 
-$insertQuery2 = "INSERT INTO books(name, description, created_at)
-                VALUES('Introduction to PHP', 'Learn all about PHP', now())";
+// $insertQuery2 = "INSERT INTO books(name, description, created_at)
+//                 VALUES('Introduction to PHP', 'Learn all about PHP', now())";
+
+$name = "PHP PDO";
+$description = "Build a basic task list";
 
 try {
-    $result = $conn->exec($insertQuery);
-    echo "$result record inserted<br>";
+    //build the query
+    $insertQuery = "INSERT INTO books (name, description, created_at)
+                VALUES (?, ?, now())";
 
-    $result = $conn->exec($insertQuery2);
-    echo "$result record inserted<br>";
+    //prepared the query
+    $statement = $conn->prepare($insertQuery);
+    //execute the statement
+    $statement->execute(array($name, $description));
+
+    echo "Record Created";
+
+    // $result = $conn->exec($insertQuery);
+    // echo "$result record inserted<br>";
+
+    // $result = $conn->exec($insertQuery2);
+    // echo "$result record inserted<br>";
 } catch (PDOException $ex) {
     echo "An error occurred " . $ex->getMessage();
 }
