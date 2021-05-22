bd4ca6ff4072bc09b0990bcb816a732b29b96131
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-05-22 20:58:25 -0300

    Binding parameters to prepared statements

--- pdo/create.php
+++ pdo/create.php
@@ -2,32 +2,27 @@
 
 include_once 'Database.php';
 
-// $insertQuery = "INSERT INTO books(name, description, created_at)
-//                 VALUES('Introduction to Java', 'Learn all about Java', now())";
-
-// $insertQuery2 = "INSERT INTO books(name, description, created_at)
-//                 VALUES('Introduction to PHP', 'Learn all about PHP', now())";
-
-$name = "PHP PDO";
-$description = "Build a basic task list";
-
 try {
     //build the query
     $insertQuery = "INSERT INTO books (name, description, created_at)
-                VALUES (?, ?, now())";
+                VALUES (:book_name, :book_description, now())";
 
     //prepared the query
     $statement = $conn->prepare($insertQuery);
-    //execute the statement
-    $statement->execute(array($name, $description));
+    
+    $statement->bindParam(":book_name", $name);
+    $statement->bindParam(":book_description", $description);
+
+    $name = "Objects and Patterns 2";
+    $description = "Software crafting";
+    $statement->execute();
+
+    $name = "Objects and Patterns 3";
+    $description = "Software crafting";
+    $statement->execute();
 
     echo "Record Created";
 
-    // $result = $conn->exec($insertQuery);
-    // echo "$result record inserted<br>";
-
-    // $result = $conn->exec($insertQuery2);
-    // echo "$result record inserted<br>";
 } catch (PDOException $ex) {
     echo "An error occurred " . $ex->getMessage();
 }
