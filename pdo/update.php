0aba936e39a71044e0fcc4d1b18685ea2e7ebff1
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-05-22 21:13:54 -0300

    Number of affected rows

--- pdo/update.php
+++ pdo/update.php
@@ -2,11 +2,30 @@
 
 include_once 'Database.php';
 
+/*
 $updateQuery = "UPDATE books SET name = 'Introduction to JAVA 2' WHERE id = 1";
 
 try {
     $result = $conn->exec($updateQuery);
     echo "$result record updated<br>";
+} catch (PDOException $ex) {
+    echo "An error occurred " . $ex->getMessage();
+}
+*/
+
+$updateQuery = "UPDATE books
+                SET name = :book_name, description = :book_desc
+                WHERE id = :id";
+try {
+    $statement = $conn->prepare($updateQuery);
+
+    $statement->execute(array(
+        ":book_name" => "Python for newbies",
+        ":book_desc" => "Introduction to software development",
+        ":id" => 15
+    ));
+
+    echo $statement->rowCount() . " record updated";
 } catch (PDOException $ex) {
     echo "An error occurred " . $ex->getMessage();
 }
