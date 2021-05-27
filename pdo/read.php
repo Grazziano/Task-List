38e3948165796833d6f183e2ad3ece9780a80d14
Grazziano <grazzianofagundes@gmail.com>
2021-05-26 21:41:06 -0300

    Updating a task with jQuery and PHP (with in-place edit)

--- pdo/read.php
+++ pdo/read.php
@@ -41,14 +41,14 @@ try {
     while ($task = $statement->fetch(PDO::FETCH_OBJ)) {
         $create_date = strftime("%b %d %Y", strtotime($task->created_at));
         $output = "<tr>
-        <td>
-            <div>$task->name</div>
+        <td title='Click to edit'>
+            <div class='editable' onClick=\"makeElementEditable(this)\" onblur=\"updateTaskName(this, '{$task->id}')\">$task->name</div>
         </td>
-        <td>
-            <div> $task->description </div>
+        <td title='Click to edit'>
+            <div class='editable' onClick=\"makeElementEditable(this)\" onblur=\"updateTaskDescription(this, '{$task->id}')\"> $task->description </div>
         </td>
-        <td>
-            <div>$task->status</div>
+        <td title='Click to edit'>
+            <div class='editable' onClick=\"makeElementEditable(this)\" onblur=\"updateTaskStatus(this, '{$task->id}')\">$task->status</div>
         </td>
         <td>$create_date</td>
         <td style='width: 5%;'><button><i class='btn-danger fa fa-times'></i></button>
